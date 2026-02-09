c8cf6f6c44f80e3db33606b95db8d6187d3d67ea
kmafeni04 <komemafeni944@gmail.com>
2026-02-09 19:26:34 +0100

    Update header
    Indicate that services is meant to be a dropdown

--- src/components/Header/component.tsx
+++ src/components/Header/component.tsx
@@ -6,18 +6,22 @@ import logo from "./assets/pedodent-logo.svg";
 
 export default function Header() {
   return (
-    <header>
-      <Link to="/">
-        <img src={logo} alt="" />
-      </Link>
-      <nav>
-        <Link to="about-us">About Us</Link>
-        <Link to="services">Services</Link>
-      </nav>
+    <>
+      <header>
+        <Link to="/">
+          <img src={logo} alt="" />
+        </Link>
+        <nav>
+          <Link to="about-us">About Us</Link>
+          {/*Dropdown*/}
+          <p>Services</p>
+        </nav>
 
-      <Link to={"/contact-us"} className="btn">
-        Contact us
-      </Link>
-    </header>
+        <Link to={"/contact-us"} className="btn">
+          Contact us
+        </Link>
+      </header>
+      <div className="header-separator"></div>
+    </>
   );
 }
